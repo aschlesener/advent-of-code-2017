d487dd33104c2f2238bfc3b003ca06dfbeee4b1a
Amy Schlesener <amy.schlesener@experticity.com>
2017-12-04 13:28:16 -0800

    Day 4 Part 2

--- day4/passphrase-validation.go
+++ day4/passphrase-validation.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -18,7 +19,9 @@ func main() {
 
 	// calculate how many passphrases are valid
 	numValidPart1 := countValidPassphrasesPart1(passphrases)
+	numValidPart2 := countValidPassphrasesPart2(passphrases)
 	fmt.Println("Number of valid passphrases for part 1 is:", numValidPart1)
+	fmt.Println("Number of valid passphrases for part 2 is:", numValidPart2)
 }
 
 // helper function to parse text file containing list of passphrases
@@ -79,4 +82,58 @@ func countValidPassphrasesPart1(passphrases [][]string) int {
 		}
 	}
 	return numValid
+}
+
+/*
+	Part 2 Rules:
+	For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+	For example:
+
+	abcde fghij is a valid passphrase.
+	abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+	a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+	iiii oiii ooii oooi oooo is valid.
+	oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+	Under this new system policy, how many passphrases are valid?
+*/
+func countValidPassphrasesPart2(passphrases [][]string) int {
+	numValid := 0
+	for _, passphraseSet := range passphrases {
+		// for each passphrase set, use map to keep track of which sorted words are already in set
+		passMap := make(map[string]int)
+		valid := true
+		for _, passphrase := range passphraseSet {
+			sortedPassphrase := sortStringByCharacter(passphrase)
+			if passMap[sortedPassphrase] == 0 {
+				passMap[sortedPassphrase] = 1
+			} else {
+				// duplicated anagrams in passphrase, not valid
+				valid = false
+			}
+		}
+		if valid {
+			numValid++
+		}
+	}
+	return numValid
+}
+
+// helper funtions for sorting charaters within a string
+// https://siongui.github.io/2017/05/07/go-sort-string-slice-of-rune/
+func stringToRuneSlice(s string) []rune {
+	var r []rune
+	for _, runeValue := range s {
+		r = append(r, runeValue)
+	}
+	return r
+}
+
+func sortStringByCharacter(s string) string {
+	r := stringToRuneSlice(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
 }
